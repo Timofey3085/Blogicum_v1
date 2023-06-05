e5a90448d974306432e94f216a1a3d1596329d5a
Тимофей Разборщиков <ash_tray85@mail.ru>
2023-06-05 13:17:01 +0300

    Сhange main.py

--- blogicum/pages/views.py
+++ blogicum/pages/views.py
@@ -1,9 +1,10 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 
-def about(request):
+def about(request) -> HttpResponse:
     return render(request, 'pages/about.html')
 
 
-def rules(request):
+def rules(request) -> HttpResponse:
     return render(request, 'pages/rules.html')
